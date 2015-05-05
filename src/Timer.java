92044240f1541e4659fd3d65408426d4ca87fad8
ashklyarovjr <anton.shklyarov@gmail.com>
2015-05-05 16:14:05 +0300

    Comments added

--- src/Timer.java
+++ src/Timer.java
@@ -1,51 +1,146 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- * Created by Anton_Shkliarov on 4/28/2015.
- */
+
 public class Timer {
     public static void main(String[] args) {
-        List<String> linkedList = Arrays.asList("a","v","b","t","ty","w","p","l");
-        List<String> list = Arrays.asList("a", "v", "b", "t", "ty", "w", "p", "l");
+        LinkedList<Integer> linkedList = (LinkedList<Integer>) linkedListInit(1000000);
+        List<Integer> arrayList = arrayListInit(1000000);
+
+        linkedListAdd(linkedList);
+        arrayListAdd(arrayList);
+        System.out.println();
+        linkedListRemove(linkedList);
+        arrayListRemove(arrayList);
+        System.out.println();
 
     }
 
-    public static long linkedListAdd(List list) {
-        long start = System.currentTimeMillis();
-        
 
+    /**
+     * Initialisation of linked list
+     * @param length - number of elements in the list
+     * */
+    public static List<Integer> linkedListInit(int length) {
 
-        long finish = System.currentTimeMillis();
-        return finish - start;
+        List<Integer> list = new LinkedList<>();
+        long start = System.nanoTime();
+        for (int i = 0; i < length; i++) {
+            list.add(i);
+        }
+        long finish = System.nanoTime();
+        System.out.println("Linked List init time: " + (finish - start) + " ns");
+        return list;
     }
 
-    public static long arrayListAdd(List list) {
-        long start = System.currentTimeMillis();
 
+    /**
+     * Initialisation of array list
+     * @param length - number of elements in the list
+     * */
+    public static List<Integer> arrayListInit(int length) {
 
-
-        long finish = System.currentTimeMillis();
-        return finish - start;
+        List<Integer> list = new ArrayList<>();
+        long start = System.nanoTime();
+        for (int i = 0; i < length; i++) {
+            list.add(i);
+        }
+        long finish = System.nanoTime();
+        System.out.println("Array List init time: " + (finish - start) + " ns");
+        return list;
     }
 
-    public static long linkedListRemove(List list) {
-        long start = System.currentTimeMillis();
 
+    /**
+     * Performing "add" operation in the beginning, middle and end of the linked list
+     * @param list - performing operations on that list
+     * */
+    public static void linkedListAdd(LinkedList<Integer> list) {
 
+        long start = System.nanoTime();
+        list.addFirst(1);
+        long finish = System.nanoTime();
+        System.out.println("Add time of first element in linked list: " + (finish - start) + " ns");
 
-        long finish = System.currentTimeMillis();
-        return finish - start;
+        long start1 = System.nanoTime();
+        list.add(list.size()/2, 1);
+        long finish1 = System.nanoTime();
+        System.out.println("Add time of middle element in linked list: " + (finish1 - start1) + " ns");
+
+        long start2 = System.nanoTime();
+        list.addLast(1);
+        long finish2 = System.nanoTime();
+        System.out.println("Add time of last element in linked list: " + (finish2 - start2) + " ns");
     }
 
-    public static long arrayListRemove(List list) {
-        long start = System.currentTimeMillis();
+
+    /**
+     * Performing "add" operation in the beginning, middle and end of the array list
+     * @param list - performing operations on that list
+     * */
+    public static void arrayListAdd(List<Integer> list) {
+
+        long start = System.nanoTime();
+        list.add(0,1);
+        long finish = System.nanoTime();
+        System.out.println("Add time of first element in array list: " + (finish - start) + " ns");
+
+        long start1 = System.nanoTime();
+        list.add(list.size()/2, 1);
+        long finish1 = System.nanoTime();
+        System.out.println("Add time of middle element in array list: " + (finish1 - start1) + " ns");
+
+        long start2 = System.nanoTime();
+        list.add(list.size(), 1);
+        long finish2 = System.nanoTime();
+        System.out.println("Add time of last element in array list: " + (finish2 - start2) + " ns");
+
+    }
 
 
+    /**
+     * Performing "remove" operation in the beginning, middle and end of the linked list
+     * @param list - performing operations on that list
+     * */
+    public static void linkedListRemove(LinkedList<Integer> list) {
 
-        long finish = System.currentTimeMillis();
-        return finish - start;
+        long start = System.nanoTime();
+        list.removeFirst();
+        long finish = System.nanoTime();
+        System.out.println("Remove time of first element in linked list: " + (finish - start) + " ns");
+
+        long start1 = System.nanoTime();
+        list.remove(list.size() / 2);
+        long finish1 = System.nanoTime();
+        System.out.println("Remove time of middle element in linked list: " + (finish1 - start1) + " ns");
+
+        long start2 = System.nanoTime();
+        list.removeLast();
+        long finish2 = System.nanoTime();
+        System.out.println("Remove time of last element in linked list: " + (finish2 - start2) + " ns");
+    }
+
+
+    /**
+     * Performing "remove" operation in the beginning, middle and end of the array list
+     * @param list - performing operations on that list
+     * */
+    public static void arrayListRemove(List<Integer> list) {
+
+        long start = System.nanoTime();
+        list.remove(0);
+        long finish = System.nanoTime();
+        System.out.println("Remove time of first element in array list: " + (finish - start) + " ns");
+
+        long start1 = System.nanoTime();
+        list.remove(list.size() / 2);
+        long finish1 = System.nanoTime();
+        System.out.println("Remove time of middle element in array list: " + (finish1 - start1) + " ns");
+
+        long start2 = System.nanoTime();
+        list.remove(list.size() - 1);
+        long finish2 = System.nanoTime();
+        System.out.println("Remove time of last element in array list: " + (finish2 - start2) + " ns");
     }
 }
